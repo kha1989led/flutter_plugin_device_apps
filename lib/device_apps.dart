56923589554f38942790f20df180ee8cf786d632
Khalid Ibrahim <kha1989led@gmail.com>
2020-05-18 14:06:29 -0700

    Added flutter_appavailability iOS code.

--- lib/device_apps.dart
+++ lib/device_apps.dart
@@ -7,6 +7,37 @@ class DeviceApps {
   static const MethodChannel _channel =
       const MethodChannel('g123k/device_apps');
 
+  static Future<Map<String, String>> checkAvailability(String uri) async {
+    Map<String, dynamic> args = <String, dynamic>{};
+    args.putIfAbsent('uri', () => uri);
+
+    if (Platform.isIOS) {
+      bool appAvailable = await _channel.invokeMethod("checkAvailability", args);
+      if (!appAvailable) {
+        throw PlatformException(code: "", message: "App not found $uri");
+      }
+      return {
+        "app_name": "",
+        "package_name": uri,
+        "versionCode": "",
+        "version_name": ""
+      };
+    }
+
+    return null;
+  }
+
+  static Future<void> launchApp(String uri) async {
+    Map<String, dynamic> args = <String, dynamic>{};
+    args.putIfAbsent('uri', () => uri);
+    if (Platform.isIOS) {
+      bool appAvailable = await _channel.invokeMethod("launchApp", args);
+      if (!appAvailable) {
+        throw PlatformException(code: "", message: "App not found $uri");
+      }
+    }
+  }
+
   static Future<List<Application>> getInstalledApplications(
       {bool includeSystemApps: false,
       bool includeAppIcons: false,
